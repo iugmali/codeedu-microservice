c4858d3db1508913e85d96f569d9b90e9db1b2d7
Guilherme Almeida <iugmali@gmail.com>
2021-02-18 00:46:03 -0300

    add navbar
    Former-commit-id: b59cb06b562f5ca796bb0a46991d665c68067604

--- www/frontend/src/App.tsx
+++ www/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import {Button} from "@material-ui/core";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and bla to reload.
+            <Button color={"primary"} variant={"contained"}>hello</Button>
         </p>
         <a
           className="App-link"
