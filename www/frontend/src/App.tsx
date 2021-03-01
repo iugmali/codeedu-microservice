e5e656da5d0971231987a068fc1adc3ecb63b831
Guilherme Almeida <iugmali@gmail.com>
2021-03-01 14:09:23 -0300

    add react router

--- www/frontend/src/App.tsx
+++ www/frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import {Navbar} from "./components/Navbar";
+import {Page} from "./components/Page";
+import {Box} from "@material-ui/core";
 
 const App: React.FC = () => {
-  return (
-    <Navbar/>
-  );
+    return (
+        <React.Fragment>
+            <Navbar/>
+            <Box paddingTop={"70px"}>
+                <Page title={"Categorias"}>
+
+                </Page>
+            </Box>
+        </React.Fragment>
+    );
 }
 
 export default App;
